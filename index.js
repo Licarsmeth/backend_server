c402e03b2e112cd46aed8744f63bbaf04ad2a590
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-07-29 16:25:47 +0545

    now it's in express

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
-let http = require("http");
-let url = require("url");
-let fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-http
-  .createServer(function (req, res) {
-    let urlname = url.parse(req.url, true);
-    let filename = "." + urlname.pathname + ".html";
+app.get("/about", (req, res) => {
+  const filePath = path.join(__dirname, "about.html");
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send("File not found");
+    }
+  });
+});
 
-    //if pathname is not empty
-    if (filename != "./.html") {
-      fs.readFile(filename, function (err, data) {
-        //error in anything
-        if (err) {
-          fs.readFile("./404.html", function (err404, data404) {
-            //error if 404.html not found
-            if (err404) {
-              res.writeHead(404, { "Content-Type": "text/html" });
-              res.write("404 not found");
-              return res.end();
-            }
-            //404.html found
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.write(data404);
-            return res.end();
-          });
-        }
-        // proper path found
-        else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          return res.end();
-        }
-      });
+app.get("/contact", (req, res) => {
+  const filePath = path.join(__dirname, "contact-me.html");
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send("File not found");
     }
-    //if only localhost:8080 show index.html
-    else {
-      fs.readFile("./index.html", function (errIndex, dataIndex) {
-        if (errIndex) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          return res.end("404 not found");
-        }
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(dataIndex);
-        return res.end();
-      });
+  });
+});
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).sendFile("File not found");
     }
-  })
-  .listen(8080);
+  });
+});
+
+// Catch-all route for undefined paths
+app.get("*", (req, res) => {
+  const filePath = path.join(__dirname, "404.html");
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send("File not found");
+    }
+  });
+});
+
+app.listen(8080, () => {
+  console.log("Now I'm using Express babyyy");
+});
